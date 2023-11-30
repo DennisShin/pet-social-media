3eb8caa2e245e0d782c2039996bd5354007b6555
Sophie Gamer <sophiergamer@gmail.com>
2023-11-30 10:54:15 -0500

    [STYLE] Polishing animal card component styles

--- client/src/AnimalCard.js
+++ client/src/AnimalCard.js
@@ -1,7 +1,7 @@
 export default function AnimalCard({name, photo, gender, type, age, size, description}){
 
 return(
-  <div className="max-w-sm md:flex flex-1 justify-center object-scale-down bg-red border-slate border-8 rounded-lg " >
+  <div className="max-w-sm flex flex-1 justify-center  border-slate border-8 rounded-lg " >
         <img className="rounded-t-lg" src={photo} alt={name} />
             <div className="p-5">
                 <h3 className="mb-2 text-lg font-bold tracking-tight text-slate">{name}</h3>
