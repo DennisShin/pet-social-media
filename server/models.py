a63ce695a43ae27167fda5d7971c61b629f41e4d
Dennis Shin <dennisshin0@gmail.com>
2023-11-30 14:36:58 -0500

    add basic user authentication methods

--- server/models.py
+++ server/models.py
@@ -7,6 +7,7 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
+from config import db
 
 
 #######################################################
@@ -15,7 +16,7 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 
 # Instantiate database connection using metadata schema.
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
@@ -23,14 +24,18 @@ class User(db.Model, SerializerMixin):
     serialize_rules = ("-ownerships.user", "-adoption_applications.user")
 
     id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String())
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     zipcode = db.Column(db.Integer, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     ownerships = db.relationship("Ownership", back_populates="user")
     adoption_applications = db.relationship("AdoptionApplication", back_populates="user")
     pets = association_proxy("ownerships", "pet")
+    def is_admin(self):
+        return self.is_admin
 
 
 
