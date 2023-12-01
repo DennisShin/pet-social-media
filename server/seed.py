fcbdfacbb609d077ffd210b36be12f3d2f07c224
Dennis Shin <dennisshin0@gmail.com>
2023-12-01 13:49:04 -0500

    [ADD] New cat images

--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,20 @@
 from config import app
 from models import User, Pet, AdoptionApplication, Ownership, db
 import bcrypt
+from flask import request
 
 
 def encrypt_password(password):
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt=salt)
     return hashed_password.decode("utf-8")
+
+def random_image():
+    response = request.get("https://api.thecatapi.com/v1/images/search")
+    data = response.json()
+    print(data)
+
+
 
 User1 = User(username="John", name="John", email="<EMAIL1>", password_hash=encrypt_password("<PASSWORD>"), zipcode=123456)
 User2 = User(username="Jane", name="Jane", email="<EMAIL2>", password_hash=encrypt_password("<PASSWORD>"), zipcode = 123456)
@@ -16,12 +24,12 @@ User4 = User(username="Mike", name="Mike", email="<EMAIL4>", password_hash=encry
 
 
 Pet1 = Pet(name="Doggo", photo="http://placekitten.com/g/200/300", description="This is a dog", size="Medium", age=2, gender="Male", type="Dog", is_adoptable=True)
-Pet2 = Pet(name="Puppy", photo="http://placekitten.com/g/200/300", description="This is a puppy", size="Medium", age=4, gender="Male", type="Dog", is_adoptable=False)
-Pet3 = Pet(name="Kitty", photo="http://placekitten.com/g/200/300", description="This is a kitty", size="Medium", age=1, gender="Male", type="Cat", is_adoptable=False)
-Pet4 = Pet(name="Bird", photo="http://placekitten.com/g/200/300", description="This is a bird", size="Medium", age=1, gender="Male", type="Bird", is_adoptable=True)
-Pet5 = Pet(name="Fish", photo="http://placekitten.com/g/200/300", description="This is a fish", size="Medium", age=3, gender="Male", type="Fish", is_adoptable=False)
-Pet6 = Pet(name="Cat", photo="http://placekitten.com/g/200/300", description="This is a cat", size="Medium", age=6, gender="Male", type="Cat", is_adoptable=True)
-Pet7 = Pet(name="Dog", photo="http://placekitten.com/g/200/300", description="This is a dog", size="Medium", age=2, gender="Male", type="Dog", is_adoptable=False)
+Pet2 = Pet(name="Puppy", photo="http://placekitten.com/g/200/301", description="This is totally a puppy", size="Medium", age=4, gender="Male", type="Dog", is_adoptable=False)
+Pet3 = Pet(name="Kitty", photo="http://placekitten.com/g/200/302", description="This is a kitty", size="Medium", age=1, gender="Male", type="Cat", is_adoptable=False)
+Pet4 = Pet(name="Bird", photo="http://placekitten.com/g/200/303", description="This is totallya bird", size="Medium", age=1, gender="Male", type="Bird", is_adoptable=True)
+Pet5 = Pet(name="Fish", photo="http://placekitten.com/g/200/304", description="This is totally a fish", size="Medium", age=3, gender="Male", type="Fish", is_adoptable=False)
+Pet6 = Pet(name="Cat", photo="http://placekitten.com/g/200/305", description="This is a cat", size="Medium", age=6, gender="Male", type="Cat", is_adoptable=True)
+Pet7 = Pet(name="Dog", photo="http://placekitten.com/g/200/306", description="This is totally a dog", size="Medium", age=2, gender="Male", type="Dog", is_adoptable=False)
 
 def create_pets():
     all_pets = [Pet1, Pet2, Pet3, Pet4, Pet5, Pet6, Pet7]
